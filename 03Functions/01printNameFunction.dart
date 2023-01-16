c498c44f437ad7a2308e589f90f2829b1ca03aff
SagarPakhrinTamang <sagarpakhrin8@gmail.com>
2023-01-17 03:16:44 +0545

    new

---
+++ 03Functions/01printNameFunction.dart
@@ -0,0 +1,8 @@
+void printName() {
+  //any name can be give while creating a function.
+  print("Sagar Pakhrin Tamang");
+}
+
+void main() {
+  printName(); //calling function
+}
---
+++ 03Functions/02addWork.dart
@@ -0,0 +1,8 @@
+void add(int n1, int n2) {
+  int sum = n1 + n2;
+  print("Sum is $sum");
+}
+
+void main() {
+  add(1, 2);
+}
