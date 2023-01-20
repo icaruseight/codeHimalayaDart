1d024aae1459374f76b17b89b6c8fce672718501
SagarPakhrinTamang <sagarpakhrin8@gmail.com>
2023-01-20 12:30:00 +0545

    Parameter

---
+++ 03Functions/03FunctionParameter/01positionalParUse.dart
@@ -0,0 +1,8 @@
+void postionParameter(String name, String country) {
+  print("Hello $name you are from $country.");
+}
+
+void main() {
+  postionParameter("Sagar", "Nepal"); //passing vlaue in corrrect form
+  postionParameter("Nepal", "Sagar"); // passing value in incorrect form
+}
