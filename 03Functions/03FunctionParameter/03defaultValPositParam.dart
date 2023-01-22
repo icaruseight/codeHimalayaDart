a81a3bbc296536a8fe0f4fe33c7c74b939eae1d5
SagarPakhrinTamang <sagarpakhrin8@gmail.com>
2023-01-22 21:22:59 +0545

    update

--- 03Functions/03FunctionParameter/03defaultValPositParam.dart
+++ 03Functions/03FunctionParameter/03defaultValPositParam.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 void add(int num1, int num2, [int num3 = 0]) {
+  //num 1 and num2 value must be given and num 3 is optional.
   int sum;
   sum = num1 + num2 + num3;
   print("The sum is $sum");
