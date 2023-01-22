561579eda4c8a89991f734a220a7c9182619fbfb
SagarPakhrinTamang <sagarpakhrin8@gmail.com>
2023-01-22 21:37:16 +0545

    anonymous Functions

---
+++ 03Functions/04AnonymousFunction/02anonymousFunction2.dart
@@ -0,0 +1,8 @@
+void main() {
+  var square = (int number) {
+    //anynomous function
+    return number * number;
+  };
+  print("The saquare of 2 is ${square(2)}");
+  print("The square of 3 is ${square(3)} ");
+}
