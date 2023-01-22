312d088f6fd94f4c1ea56c01931b05f59e34474e
SagarPakhrinTamang <sagarpakhrin8@gmail.com>
2023-01-22 22:15:20 +0545

    initial

--- 03Functions/06Scope/01scope.dart
+++ 03Functions/06Scope/01scope.dart
@@ -3,3 +3,10 @@ void main() {
       "Sagar Pakhrin"; //(Local Scope) myName can be used only up to end of } this bracket which is in line number 5.
   print(myName);
 }
+
+/*
+void printName() {
+  print(
+      myName); // since the local scope is used here it myName cannot be used here
+}
+*/
