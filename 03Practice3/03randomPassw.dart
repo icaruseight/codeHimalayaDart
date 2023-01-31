8d656ba0651251b4d553be25f7713b5bea169050
SagarPakhrinTamang <sagarpakhrin8@gmail.com>
2023-01-31 15:35:37 +0545

    math

--- 03Practice3/03randomPassw.dart
+++ 03Practice3/03randomPassw.dart
@@ -1,2 +1,20 @@
 //Write a program in Dart that generates random password.
 
+import 'dart:math';
+
+String generateRandomString(int lengthOfString) {
+  final random = Random();
+  const allChars =
+      'AaBbCcDdlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1EeFfGgHhIiJjKkL234567890';
+  // below statement will generate a random string of length using the characters
+  // and length provided to it
+  final randomString = List.generate(
+          lengthOfString, (index) => allChars[random.nextInt(allChars.length)])
+      .join();
+  return randomString; // return the generated string
+}
+
+void main() {
+  print(generateRandomString(23));
+  print(generateRandomString(3));
+}
