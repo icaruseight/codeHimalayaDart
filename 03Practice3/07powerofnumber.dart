e11ea4458fcb7f979b226ccdfdefcfa971dc73dc
SagarPakhrinTamang <sagarpakhrin8@gmail.com>
2023-02-01 13:55:07 +0545

    function

--- 03Practice3/07powerofnumber.dart
+++ 03Practice3/07powerofnumber.dart
@@ -1,1 +1,14 @@
 //Write a program in Dart to calculate power of a certain number. For e.g 5^3=125
+
+import 'dart:math';
+
+void powerNum() {
+  num num1 = 2.2;
+  num num2 = 3.8;
+
+  print("$num1 to the power of $num2 is ${pow(num1, num2)}");
+}
+
+void main() {
+  powerNum();
+}
